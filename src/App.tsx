53af9637281df489b4d9f064d2fe83a5fc9ef2a7
Mouad_BEN_ABDALLAH <mouad.benabdallah1203@gmail.com>
2022-05-18 09:48:32 +0200

    Fin de la page de rendu des todos

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from './shared/ui'
+import TodoList from './todo_list/TodoList'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoList />
+    </>
+  )
+}
